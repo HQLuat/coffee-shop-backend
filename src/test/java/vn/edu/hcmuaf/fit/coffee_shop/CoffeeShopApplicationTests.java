315eab45fee7017fb9edd320c6ca026420048f0c
HQLuat <22130157@st.hcmuaf.edu.vn>
2025-11-11 23:49:19 +0700

    Initial commit: Spring Boot backend for coffee shop

---
+++ src/test/java/vn/edu/hcmuaf/fit/coffee_shop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.coffee_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

de5991096d3db6b97f974f17a65bda3355e5cf02
NhanNghia2004 <22130185@st.hcmuaf.edu.vn>
2025-12-16 22:18:28 +0700

    review- xem đánh giá sản phẩm

--- src/main/java/vn/edu/hcmuaf/fit/coffee_shop/order/repository/OrderItemRepository.java
+++ src/main/java/vn/edu/hcmuaf/fit/coffee_shop/order/repository/OrderItemRepository.java
@@ -4,7 +4,16 @@ package vn.edu.hcmuaf.fit.coffee_shop.order.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.hcmuaf.fit.coffee_shop.order.entity.OrderItem;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    // Kiểm tra xem người dùng có OrderItem này trong đơn hàng đã DELIVERED hay không
+    @Query("SELECT COUNT(oi) > 0 FROM OrderItem oi " +
+           "JOIN oi.order o " +
+           "WHERE o.user.id = :userId " +
+           "AND oi.productId = :productId " +
+           "AND o.status = vn.edu.hcmuaf.fit.coffee_shop.order.entity.OrderStatus.DELIVERED")
+    boolean hasUserBoughtProduct(@Param("userId") Long userId, @Param("productId") Long productId);
 }
